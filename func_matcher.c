6e93d8f0d2a42dc983f18d2ef19315f41b4f0db9
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-07-12 09:17:31 -0700

    fixed func_matcher betty

--- func_matcher.c
+++ func_matcher.c
@@ -10,42 +10,41 @@
 
 int get_matcher(const char *fmt, va_list args, p_fmt p_format[])
 {
-    int tag = 0, x, y, n;
+	int tag = 0, x, y, n;
 
-    if (fmt == NULL)
+		if (fmt == NULL)
 		return (-1);
-    
-    for (x = 0; fmt && fmt[x] != '\0'; x++)
-    {
-        if(fmt[x] != "%")
-        {
-            _putchar(fmt[x]);
-            tag++;
-        }else{
-            for (y = 0; p_format[y].symbol && p_format[y].symbol[0] == fmt[x + 1]; y++)
+
+	for (x = 0; fmt && fmt[x] != '\0'; x++)
+	{
+		if (fmt[x] != "%")
+	{
+		_putchar(fmt[x]);
+		tag++;
+		}
+		else
+		{
+	for (y = 0; p_format[y].symbol && p_format[y].symbol[0] == fmt[x + 1]; y++)
+	{
+		n = p_format[y].print(args);
+		tag += n;
+		x++;
+		break;
+	}
+		if (p_format[y].symbol == NULL && fmt[x + 1] != ' ')
+		{
+			if (fmt[x + 1] != 0)
 			{
-                n = p_format[y].print(args);
-                tag += n;
-                x++;
-                break;
-			}
-			if (p_format[y].symbol == NULL && fmt[x + 1] != ' ')
-			{
-				if (fmt[x + 1] != 0)
-				{
-					_putchar(fmt[x]);
-                    tag++;
-					_putchar(fmt[x + 1]);
-					tag++;
+				_putchar(fmt[x]);
+				tag++;
+				_putchar(fmt[x + 1]);
+				tag++;
 					x++;
-				}
-				else
-					return (-1);
 			}
-        }
-
-    }
-
-
+			else
+				return (-1);
+		}
+	}
+}
 	return (tag);
 }
