5efd8a03e484facab57cc8f6edaeaba9cc699f99
Ogu Johnkennedy <49487438+mathjken@users.noreply.github.com>
2022-07-12 14:30:12 +0100

    fixed indentations

--- _printf.c
+++ _printf.c
@@ -3,17 +3,17 @@
 int _printf(const char *format, ...)
 {
   int tag = 0;
-  if (format == 0)
-    return (-1);
+    if (format == 0)
+      return (-1);
 
-  p_fmt p_format[] = {
-      {"d", print_num},
-      {"i", print_num},
-      {"c", print_char},
-      {"s", print_string},
-      {"%", print_percent},
-      {NULL, NULL},
-  };
+p_fmt p_format[] = {
+  {"d", print_num},
+  {"i", print_num},
+  {"c", print_char},
+  {"s", print_string},
+  {"%", print_percent},
+  {NULL, NULL},
+};
 
   va_list args;
   va_start(args, format);
