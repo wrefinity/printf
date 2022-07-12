21393787f0c2d881a335d54aba8a44cc769bc431
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-07-12 06:50:55 -0700

    fixed betty

--- _printf.c
+++ _printf.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
+#include "main.h"
+
 int _printf(const char *format, ...)
 {
-  int tag = 0;
-    if (format == 0)
-      return (-1);
+	int tag = 0;
+		if (format == 0)
+		return (-1);
 
-p_fmt p_format[] = {
-  {"d", print_num},
-  {"i", print_num},
-  {"c", print_char},
-  {"s", print_string},
-  {"%", print_percent},
-  {NULL, NULL},
+	p_fmt p_format[] = {
+		{"d", print_num},
+		{"i", print_num},
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL},
 };
 
-  va_list args;
-  va_start(args, format);
-  tag = get_matcher(format, args, p_format);
+	va_list args;
+	va_start(args, format);
+	tag = get_matcher(format, args, p_format);
 
-  va_end(args);
-  return tag;
+	va_end(args);
+	return tag;
 }
